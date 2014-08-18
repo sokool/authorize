83de13df02937b3d425325126eff0510c1841c33
Michal Sokolowski <michal.sokolowski@smtsoftware.com>
2014-08-18 09:29:47 +0200

    unit -tests

---
+++ test/UserFactoryTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * UserFactoryTest class container
+ *
+ * @package     AuthorizeTest
+ * @copyright   2012 SMT Software S.A.
+ */
+
+namespace AuthorizeTest;
+
+use Authorize\Factory\UserFactory;
+use FloTest\Bootstrap;
+
+/**
+ * UserFactory tests
+ *
+ * @package AuthorizeTest
+ * @author  Michal Kuriata <michal.kuriata@smtsoftware.com>
+ */
+class UserFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var UserFactory
+     */
+    protected $factory;
+
+	/**
+	 * setUp method
+	 */
+	public function setUp()
+    {
+        $this->factory = new UserFactory();
+    }
+
+	/**
+	 * Testing:
+	 * createService()
+	 * UserHelper::setMvcKeeper()
+	 * UserHelper::setControllerManager()
+	 * UserHelper::setRouteStack()
+	 *
+	 * Should return configured helper
+	 */
+	public function testCreateService()
+	{
+		$sm     = Bootstrap::getServiceManager();
+		$helper = $this->factory->createService($sm->get('ControllerManager'));
+
+		$this->assertInstanceOf('Authorize\View\Helper\User', $helper);
+		$this->assertEquals($helper->getMvcKeeper(), $sm->get('Authorize\MvcKeeper'));
+		$this->assertEquals($helper->getControllerManager(), $sm->get('ControllerManager'));
+		$this->assertEquals($helper->getRouteStack(), $sm->get('Router'));
+	}
+} 
